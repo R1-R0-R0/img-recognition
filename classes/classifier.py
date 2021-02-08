c31f5bd83629e720f68b3e98bf7c924bea9b6887
Airels <yohan13240@hotmail.fr>
2021-02-08 17:00:27 +0100

    ajout fonction de test

--- classes/classifier.py
+++ classes/classifier.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
-import numpy as np;
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 class Classifier(ABC):
 
@@ -11,3 +13,34 @@ class Classifier(ABC):
 
     def reset(self):
         pass
+
+
+def classifier_test(classifier: Classifier, X: np.array, y: np.array, number_of_tests = 50, default_test_size = 0.20):
+    assert default_test_size > 0
+    assert default_test_size < 1
+    
+    classifier.reset()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=default_test_size);
+
+    classifier.fit(X_train, y_train)
+    y_predicted = classifier.predict(X_test);
+
+    print("--- Simple test ---")
+    print("Real classes : ", y_test);
+    print("Predicted classes : ", y_predicted);
+    print("Score : ", accuracy_score(y_test,y_predicted))
+
+    print()
+
+    print("--- Testing ", number_of_tests, " times ---")
+    sum = 0
+    for i in range(number_of_tests):
+        classifier.reset()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=default_test_size);
+
+        classifier.fit(X_train, y_train)
+        y_predicted = classifier.predict(X_test)
+        sum += accuracy_score(y_test, y_predicted)
+
+    print("Total score : ", (sum / number_of_tests))
--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 from classes.classifier_axiom import ClassifierAxiom
-from sklearn.model_selection import train_test_split
+from classes.classifier import classifier_test as ct
 import os
 from image import Image
 import numpy as np
-from sklearn.metrics import accuracy_score
 
 averagePercentColors = [];
 classes = [];
@@ -37,26 +36,5 @@ print("Fin traitement")
 X = np.array(averagePercentColors);
 y = np.array(classes);
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20);
-
 classifier = ClassifierAxiom(['averageRedPercent', 'averageGreenPercent', 'averageBluePercent'])
-classifier.fit(X_train, y_train)
-
-y_predicted = classifier.predict(X_test);
-print("Real classes :");
-print(y_test);
-print("Predicted classes :");
-print(y_predicted);
-
-print("Taux de réussite : ", accuracy_score(y_test,y_predicted))
-
-sum = 0
-for i in range(1000):
-    classifier = ClassifierAxiom(['averageRedPercent', 'averageGreenPercent', 'averageBluePercent'])
-    classifier.fit(X_train, y_train)
-
-    y_predicted = classifier.predict(X_test)
-    sum += accuracy_score(y_test, y_predicted)
-
-print("Score:")
-print(sum / 1000)
+ct(classifier, X, y, number_of_tests=1000)
