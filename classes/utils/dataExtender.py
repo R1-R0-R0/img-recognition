64d5ef9f86492260b93c7d1be7935fa75d155f1c
Airels <yohan13240@hotmail.fr>
2021-02-22 15:33:45 +0100

    refactor data_extender + clear

---
+++ classes/utils/dataExtender.py
@@ -0,0 +1,91 @@
+import os
+from random import random, Random
+import cv2
+# from PIL import Image
+from math import ceil
+
+
+noisy_delta = 30
+mirror_extension = '_mirror/'
+noise_extension = '_noise_' + str(noisy_delta) + '/'
+random_generator = Random()
+
+# ---
+
+def createMirroredImage(dirname, filename, originalImage):
+    newPath = dirname + mirror_extension + filename
+    if os.path.exists(newPath):
+        return
+
+    mirroredImage = cv2.flip(originalImage, 1)
+    cv2.imwrite(newPath, mirroredImage)
+
+# ---
+
+def createNoisyImage(dirname, filename, originalImage):
+    newPath = dirname + noise_extension + filename
+    if os.path.exists(newPath):
+        return
+    
+    red, green, blue = cv2.split(originalImage)
+    colors = [red, green, blue]
+    for color in colors:
+        for pixel in color:
+            for i in range(len(pixel)):
+                randomDelta = random_generator.randint(-noisy_delta, noisy_delta)
+                tempPixelColor = pixel[i] + randomDelta
+                tempPixelColor = 0 if tempPixelColor < 0 else tempPixelColor
+                tempPixelColor = tempPixelColor if tempPixelColor < 256 else 255
+                pixel[i] = tempPixelColor
+                    
+    noisyImage = cv2.merge(colors)
+    cv2.imwrite(newPath, noisyImage)
+
+# ---
+
+def createExtendedDirectories():
+    if not os.path.exists('./Data/Mer' + mirror_extension):
+        os.makedirs('./Data/Mer' + mirror_extension)
+    if not os.path.exists('./Data/Ailleurs' + mirror_extension):
+        os.makedirs('./Data/Ailleurs' + mirror_extension)
+
+    if not os.path.exists('./Data/Mer' + noise_extension):
+        os.makedirs('./Data/Mer' + noise_extension)
+    if not os.path.exists('./Data/Ailleurs' + noise_extension):
+        os.makedirs('./Data/Ailleurs' + noise_extension)
+
+def createExtendedImage(dirname, filename):
+    originalImage = cv2.imread(dirname + '/' + filename, cv2.IMREAD_UNCHANGED)
+    try:
+        createMirroredImage('./Data/Mer', filename, originalImage)
+    except:
+        pass
+    
+    try:
+        createNoisyImage('./Data/Mer', filename, originalImage)
+    except:
+        pass
+
+def createExtendedImages():
+    createExtendedDirectories()
+
+    listMer = os.listdir('./Data/Mer')
+    listAilleurs = os.listdir('./Data/Ailleurs')
+    numberOfImages = len(listMer) + len(listAilleurs)
+    counter = 0
+
+    print("Creating extended 'Mer' images...")
+    for file in listMer:
+        createExtendedImage('./Data/Mer', file)
+        counter += 1
+        print(ceil((counter / numberOfImages)*100), '%', end = '\r')
+
+    print("Creating extended 'Ailleurs' images... ")
+    for file in listAilleurs:
+        createExtendedImage('./Data/Ailleurs', file)
+        counter += 1
+        print(ceil((counter / numberOfImages)*100), '%', end = '\r')
+
+
+# Renommer classifier_axiom en classifier_multi_axiom
+# Faire un combiner précoce et tardif
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from classes.image import Image
 import os
 from classes.classifier import classifier_test
 from math import ceil
+import classes.utils.dataExtender as DataExtender
 
 class Main:
 
@@ -113,13 +114,15 @@ def loadPartialData(numbersOfImages, displayLoadingFile = False):
 # - Instancier cette classe dans listDescriptors
 # - Tester votre descripteur avec le code ci-dessous en l'appelant par son nom défini
 if __name__ == '__main__':
-    X, y = load('data')
+    # X, y = load('data')
     # X, y = loadPartialData(10)
     # save('test_data_2.R0', X, y)
-    classifier = ClassifierAxiom('PixelArrayResize')
+    # classifier = ClassifierAxiom('PixelArrayResize')
     # classifier_test(classifier, X, y, 100, 0.20, True)
     # classifier = ClassifierCombine()
     # classifier.addClassifier(ClassifierAxiom('PercentColors'))
     # classifier.addClassifier(ClassifierAxiom('PixelArrayResize'))
     # classifier.addClassifier(ClassifierAxiom('ColorContrast'))
-    classifier_test(classifier, X, y, 100, 0.20, True)
+    # classifier_test(classifier, X, y, 100, 0.20, True)
+    # DataExtender.createExtendedImage('./Data/Mer', '838s.jpg')
+    DataExtender.createExtendedImages()
